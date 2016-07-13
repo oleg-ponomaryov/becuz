d3dd845100a387bcd958990dd214c758c53683ea
Hai Nguyen <hai.nguyen@jeppesen.com>
2016-07-12 20:32:08 -0600

    Remove frame descriptions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,21 +4,21 @@ VALUES ('abc-edcv','demo@quantlance.com', '$2a$10$ebyC4Z5WtCXXc.HGDc1Yoe6CLFzcnt
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
   values ('4684db9b-c723-4fac-93bc-a9948019f06','2016-07-06 04:46:56.733',null,
-  'Jeff will provide description for this frame.',null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/zep.png',
+  null,null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/zep.png',
   0,0,null,'http://ec2-54-183-19-16.us-west-1.compute.amazonaws.com:8081/framesvc/frames/zep/',
   'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/zep_window.png');
 
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
   values ('4684db9b-c723-4fac-93bc-a9948019f0a7','2016-07-06 04:46:56.733',null,
-  'Jeff will provide description for this frame.',null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/bus.png',
+  null,null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/bus.png',
   0,0,null,'http://ec2-54-183-19-16.us-west-1.compute.amazonaws.com:8081/framesvc/frames/bus/',
   'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/bus_window.png');
 
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
   values ('4684db9b-c723-4fac-93bc-a9948019f0a8','2016-07-06 04:46:56.733',null,
-  'Jeff will provide description for this frame.',null,'true',0,0,
+  null,null,'true',0,0,
   'https://s3-us-west-1.amazonaws.com/becuz/images/frames/goldframe.png',0,0,null,
   'http://ec2-54-183-19-16.us-west-1.compute.amazonaws.com:8081/framesvc/frames/goldframe/',
   'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/golden_window.png');
