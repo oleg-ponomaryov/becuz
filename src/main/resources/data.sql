57f2be25ee22dec26b7b1452e7df46e2ffee12b0
Hai Nguyen <hai.nguyen@jeppesen.com>
2016-07-14 10:21:44 -0600

    Change the Frame URL to the external domain URL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,14 +5,14 @@ Insert into frames (id,created,child_id,description,display_type,isdefault,photo
   preview_url,priority,status,updated,url,window_url) 
   values ('4684db9b-c723-4fac-93bc-a9948019f06','2016-07-06 04:46:56.733',null,
   null,null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/zep.png',
-  0,0,null,'http://ec2-54-183-19-16.us-west-1.compute.amazonaws.com:8081/framesvc/frames/zep/',
+  0,0,null,'http://frames.becuz.co/framesvc/frames/zep/',
   'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/zep_window.png');
 
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
   values ('4684db9b-c723-4fac-93bc-a9948019f0a7','2016-07-06 04:46:56.733',null,
   null,null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/bus.png',
-  0,0,null,'http://ec2-54-183-19-16.us-west-1.compute.amazonaws.com:8081/framesvc/frames/bus/',
+  0,0,null,'http://frames.becuz.co/framesvc/frames/bus/',
   'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/bus_window.png');
 
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
@@ -20,5 +20,5 @@ Insert into frames (id,created,child_id,description,display_type,isdefault,photo
   values ('4684db9b-c723-4fac-93bc-a9948019f0a8','2016-07-06 04:46:56.733',null,
   null,null,'true',0,0,
   'https://s3-us-west-1.amazonaws.com/becuz/images/frames/goldframe.png',0,0,null,
-  'http://ec2-54-183-19-16.us-west-1.compute.amazonaws.com:8081/framesvc/frames/goldframe/',
+  'http://frames.becuz.co/framesvc/frames/goldframe/',
   'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/golden_window.png');
