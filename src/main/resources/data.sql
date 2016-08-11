dd796a522743b2d09a5b8a30c5e45b6b6cfa33af
Hai Nguyen <hai.nguyen@jeppesen.com>
2016-08-11 15:41:32 -0600

    Update URLs of the frames as they have been move to a new bucket

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,10 +8,10 @@ WHERE
 
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
-  SELECT '4684db9b-c723-4fac-93bc-a9948019f06','2016-07-06 04:46:56.733',null,
-  null,null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/zep.png',
-  0,0,null,'http://frames.becuz.co/framesvc/frames/zep/',
-  'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/zep_window.png'
+  SELECT '4684db9b-c723-4fac-93bc-a9948019f06','2016-07-06 04:46:56.733',null,  
+  null,null,'false',0,0,'http://www.becuz-frames.net/preview-frames/zep.png',
+  0,0,null,'http://www.becuz-frames.net/frames/zep/index.html',
+  'http://www.becuz-frames.net/photo-windows/zep_window.png'
 WHERE
     NOT EXISTS (
         SELECT id FROM frames WHERE id = '4684db9b-c723-4fac-93bc-a9948019f06'
@@ -20,9 +20,9 @@ WHERE
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
   SELECT '4684db9b-c723-4fac-93bc-a9948019f0a7','2016-07-06 04:46:56.733',null,
-  null,null,'false',0,0,'https://s3-us-west-1.amazonaws.com/becuz/images/frames/bus.png',
-  0,0,null,'http://frames.becuz.co/framesvc/frames/bus/',
-  'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/bus_window.png'
+  null,null,'false',0,0,'http://www.becuz-frames.net/preview-frames/bus.png',
+  0,0,null,'http://www.becuz-frames.net/frames/bus/index.html',
+  'http://www.becuz-frames.net/photo-windows/bus_window.png'
 WHERE
     NOT EXISTS (
         SELECT id FROM frames WHERE id = '4684db9b-c723-4fac-93bc-a9948019f0a7'
@@ -32,26 +32,26 @@ Insert into frames (id,created,child_id,description,display_type,isdefault,photo
   preview_url,priority,status,updated,url,window_url) 
   SELECT '4684db9b-c723-4fac-93bc-a9948019f0a8','2016-07-06 04:46:56.733',null,
   null,null,'true',0,0,
-  'https://s3-us-west-1.amazonaws.com/becuz/images/frames/goldframe.png',0,0,null,
-  'http://frames.becuz.co/framesvc/frames/goldframe/',
-  'https://s3-us-west-1.amazonaws.com/becuz/images/photo_windows/golden_window.png'
+  'http://www.becuz-frames.net/preview-frames/goldframe.png',0,0,null,
+  'http://www.becuz-frames.net/frames/goldframe/index.html',
+  'http://www.becuz-frames.net/photo-windows/golden_window.png'
   WHERE
     NOT EXISTS (
         SELECT id FROM frames WHERE id = '4684db9b-c723-4fac-93bc-a9948019f0a8'
     );
 
-    update frames set preview_url = 'http://s3-us-west-1.amazonaws.com/becuz-frames/preview-frames/goldframe.png',
-    						  url ='http://s3-us-west-1.amazonaws.com/becuz-frames/frames/golden/index.html',
-    					window_url ='http://s3-us-west-1.amazonaws.com/becuz-frames/photo-windows/golden_window.png'
+    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/goldframe.png',
+    						  url ='http://www.becuz-frames.net/frames/golden/index.html',
+    					window_url ='http://www.becuz-frames.net/photo-windows/golden_window.png'
     where id = '4684db9b-c723-4fac-93bc-a9948019f0a8';					
     				
 
-    update frames set preview_url = 'http://s3-us-west-1.amazonaws.com/becuz-frames/preview-frames/bus.png',
-    						  url ='http://s3-us-west-1.amazonaws.com/becuz-frames/frames/bus/index.html',
-    					window_url ='http://s3-us-west-1.amazonaws.com/becuz-frames/photo-windows/bus_window.png'
+    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/bus.png',
+    						  url ='http://www.becuz-frames.net/frames/bus/index.html',
+    					window_url ='http://www.becuz-frames.net/photo-windows/bus_window.png'
     where id = '4684db9b-c723-4fac-93bc-a9948019f0a7';					
 
-    update frames set preview_url = 'http://s3-us-west-1.amazonaws.com/becuz-frames/preview-frames/zep.png',
-    						  url ='http://s3-us-west-1.amazonaws.com/becuz-frames/frames/zep/index.html',
-    					window_url ='http://s3-us-west-1.amazonaws.com/becuz-frames/photo-windows/zep_window.png'
+    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/zep.png',
+    						  url ='http://www.becuz-frames.net/frames/zep/index.html',
+    					window_url ='http://www.becuz-frames.net/photo-windows/zep_window.png'
     where id = '4684db9b-c723-4fac-93bc-a9948019f0a7';					
