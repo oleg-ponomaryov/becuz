b3d11e17e0930197ba11857e786b389516604d4f
Hai Nguyen <hai.nguyen@jeppesen.com>
2016-08-18 09:52:20 -0600

    Update frames urls

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ WHERE
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
   SELECT '4684db9b-c723-4fac-93bc-a9948019f06','2016-07-06 04:46:56.733',null,  
-  null,null,'false',0,0,'http://www.becuz-frames.net/preview-frames/zep.png',
+  null,null,'false',0,0,'http://www.becuz-frames.net/preview-frames/zep.jpg',
   0,0,null,'http://www.becuz-frames.net/frames/zep/index.html',
   'http://www.becuz-frames.net/photo-windows/zep_window.png'
 WHERE
@@ -20,7 +20,7 @@ WHERE
 Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
   preview_url,priority,status,updated,url,window_url) 
   SELECT '4684db9b-c723-4fac-93bc-a9948019f0a7','2016-07-06 04:46:56.733',null,
-  null,null,'false',0,0,'http://www.becuz-frames.net/preview-frames/bus.png',
+  null,null,'false',0,0,'http://www.becuz-frames.net/preview-frames/bus.jpg',
   0,0,null,'http://www.becuz-frames.net/frames/bus/index.html',
   'http://www.becuz-frames.net/photo-windows/bus_window.png'
 WHERE
@@ -32,7 +32,7 @@ Insert into frames (id,created,child_id,description,display_type,isdefault,photo
   preview_url,priority,status,updated,url,window_url) 
   SELECT '4684db9b-c723-4fac-93bc-a9948019f0a8','2016-07-06 04:46:56.733',null,
   null,null,'true',0,0,
-  'http://www.becuz-frames.net/preview-frames/goldframe.png',0,0,null,
+  'http://www.becuz-frames.net/preview-frames/goldframe.jpg',0,0,null,
   'http://www.becuz-frames.net/frames/goldframe/index.html',
   'http://www.becuz-frames.net/photo-windows/golden_window.png'
   WHERE
@@ -40,18 +40,18 @@ Insert into frames (id,created,child_id,description,display_type,isdefault,photo
         SELECT id FROM frames WHERE id = '4684db9b-c723-4fac-93bc-a9948019f0a8'
     );
 
-    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/goldframe.png',
+    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/goldframe.jpg',
     						  url ='http://www.becuz-frames.net/frames/golden/index.html',
     					window_url ='http://www.becuz-frames.net/photo-windows/golden_window.png'
     where id = '4684db9b-c723-4fac-93bc-a9948019f0a8';					
     				
 
-    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/bus.png',
+    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/bus.jpg',
     						  url ='http://www.becuz-frames.net/frames/bus/index.html',
     					window_url ='http://www.becuz-frames.net/photo-windows/bus_window.png'
     where id = '4684db9b-c723-4fac-93bc-a9948019f0a7';					
 
-    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/zep.png',
+    update frames set preview_url = 'http://www.becuz-frames.net/preview-frames/zep.jpg',
     						  url ='http://www.becuz-frames.net/frames/zep/index.html',
     					window_url ='http://www.becuz-frames.net/photo-windows/zep_window.png'
     where id = '4684db9b-c723-4fac-93bc-a9948019f0a7';					
