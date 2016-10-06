e452a9aa005082a67d250e47730d7e6c31df8d37
Hai Nguyen <hai.nguyen@jeppesen.com>
2016-10-06 09:54:57 -0600

    Added the vintagetv frame

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -73,6 +73,22 @@ WHERE
         SELECT id FROM frames WHERE id = '4684db9b-c723-4fac-93bc-a9948019f1b'
     );
     
+Insert into frames (id,created,child_id,description,display_type,isdefault,photo_height,photo_width,
+  preview_url,priority,status,updated,url,window_url) 
+  SELECT '4684db9b-c723-4fac-93bc-a9948019f2b','2016-07-06 04:46:56.733',null,  
+  null,null,'false',0,0,'http://my.becuz.co/preview-frames/vintagetv.jpg',
+  0,0,null,'http://my.becuz.co/frames/vintagetv/index.html',
+  'http://my.becuz.co/photo-windows/vintagetv_window.png'
+WHERE
+    NOT EXISTS (
+        SELECT id FROM frames WHERE id = '4684db9b-c723-4fac-93bc-a9948019f2b'
+    );
+    
+    update frames set preview_url = 'http://my.becuz.co/preview-frames/vintaget.jpg',
+    						  url ='http://my.becuz.co/frames/clouds/index.html',
+    					window_url ='http://my.becuz.co/photo-windows/vintaget_window.png'
+    where id = '4684db9b-c723-4fac-93bc-a9948019f2b';
+    
     update frames set preview_url = 'http://my.becuz.co/preview-frames/clouds.jpg',
     						  url ='http://my.becuz.co/frames/clouds/index.html',
     					window_url ='http://my.becuz.co/photo-windows/clouds_window.png'
